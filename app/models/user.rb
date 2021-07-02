34c98802457447f40976aea6618a5c69cfc187e7
Robert Heath <robert.b.heathii@gmail.com>
2021-07-02 11:04:23 -0500

    Change user format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :organizations
 
-  validates_presence_of :first_name, :last_name, :phone_number, :email
+  validates_presence_of :first_name,
+                        :last_name,
+                        :phone_number,
+                        :email
 end
