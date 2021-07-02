e8ce82a4669de864fbfb2f011d01d12aea47553a
Robert Heath <robert.b.heathii@gmail.com>
2021-07-02 11:43:59 -0500

    Create employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,19 @@
-class Employee < User
+class Employee < ApplicationRecord
   belongs_to :organization
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :phone_number,
+                        :email,
+                        :full_legal_name,
+                        :street,
+                        :city,
+                        :state,
+                        :zip,
+                        :ssn,
+                        :union_designee,
+                        :employment_status
+
+  enum employment_status: [:full_time, :part_time, :contract]
+  enum passport_sex: [:male, :female, :other]
 end
