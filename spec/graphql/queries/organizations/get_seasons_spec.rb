ec7e5a4e1fb8f71411e4004dc056b2cec48d471d
Robert Heath <robert.b.heathii@gmail.com>
2021-07-17 13:27:57 -0500

    Test for seasons across different orgs

--- spec/graphql/queries/organizations/get_seasons_spec.rb
+++ spec/graphql/queries/organizations/get_seasons_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Types::QueryType, type: :request do
   describe 'get seasons for an organization' do
     let(:user) { create(:user) }
     let(:org) { create(:organization, user: user) }
+    let(:org2) { create(:organization, user: user) }
     let(:season) { create(:season, organization: org) }
+    let(:season2) { create(:season, organization: org2) }
     let(:query_type_all) { "seasons" }
     let(:query_string_all) { <<~GQL
       query organization($id: ID!) {
@@ -14,6 +16,9 @@ RSpec.describe Types::QueryType, type: :request do
           seasons {
             id
             name
+            organization {
+              id
+            }
             isArchived
             startDate
             endDate
@@ -44,8 +49,10 @@ RSpec.describe Types::QueryType, type: :request do
           "startDate" => season.start_date.to_s,
           "endDate" => season.end_date.to_s,
           "isArchived" => season.is_archived,
-          "description" => season.description
+          "description" => season.description,
+          "organization" => { "id" => org.id.to_s }
         }])
+        expect(gql_response.data["organization"]["seasons"].last["organization"]["id"]).to_not eq(org2.id)
       end
     end
 
