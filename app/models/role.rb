a2d73498efe608f61db4c69714d0fd67dcf1cf84
Robert Heath <robert.b.heathii@gmail.com>
2021-11-09 16:16:26 -0600

    Note on role for future thought

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,5 @@ class Role < ApplicationRecord
   has_many :employees, through: :employee_roles
 
   validates_presence_of :name
+  #The thing I'm trying to figure out: should roles belong to an organization or should they be above the organization. Should the organization be able to customize them?
 end
